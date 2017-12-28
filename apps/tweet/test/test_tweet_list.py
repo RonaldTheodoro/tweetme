369e2699a37e236d58d7599f304414002e2901f4
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-27 23:12:44 -0200

    add test_context, test_is_instance_of, test_equal and test_count in TweetListViewTest

--- apps/tweet/test/test_tweet_list.py
+++ apps/tweet/test/test_tweet_list.py
@@ -1,15 +1,25 @@
-from django.urls import resolve, reverse
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import resolve, reverse
 
 from rest_framework import status
 
-from .. import views
+from .. import models, views
+
+
+User = get_user_model()
 
 
 class TweetListViewTest(TestCase):
     url = reverse('tweet:list')
 
     def setUp(self):
+        user = User.objects.create_user(username='ronaldtheodoro')
+        self.tweets = [
+            models.Tweet.objects.create(content='My First Tweet', user=user),
+            models.Tweet.objects.create(content='My Second Tweet', user=user),
+            models.Tweet.objects.create(content='My Third Tweet', user=user),
+        ]
         self.response = self.client.get(self.url)
 
     def test_get(self):
@@ -21,3 +31,17 @@ class TweetListViewTest(TestCase):
     def test_function(self):
         index = resolve(self.url)
         self.assertEqual(views.tweet_list, index.func)
+
+    def test_context(self):
+        self.assertIn('tweets', self.response.context)
+
+    def test_is_instance_of(self):
+        with self.subTest():
+            for tweet in self.response.context['tweets']:
+                self.assertIsInstance(tweet, models.Tweet)
+    
+    def test_equal(self):
+        self.assertEqual(self.response.context['tweets'], self.tweets)
+
+    def test_count(self):
+        self.assertEqual(len(self.response.context['tweets']), 3)
