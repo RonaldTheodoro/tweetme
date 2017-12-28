651656723309f1d53165da2dd412896e0a3f56c4
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-27 22:05:57 -0200

    add user foreign key

--- apps/tweet/models.py
+++ apps/tweet/models.py
@@ -1,9 +1,16 @@
+from django.conf import settings
 from django.db import models
 
 from model_utils.models import TimeStampedModel
 
 class Tweet(TimeStampedModel):
     content = models.CharField('content', max_length=140)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='tweet',
+        verbose_name='user'
+    )
 
     def __str__(self):
         return str(self.content)
--- apps/tweet/test/test_models.py
+++ apps/tweet/test/test_models.py
@@ -1,11 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from .. import models
 
+
+User = get_user_model()
+
+
 class TweetModelTest(TestCase):
 
     def setUp(self):
-        self.tweet = models.Tweet.objects.create(content='My first tweet')
+        user = User.objects.create_user(username='ronaldtheodoro')
+        self.tweet = models.Tweet.objects.create(
+            content='My first tweet',
+            user=user
+        )
 
     def test_exists(self):
         self.assertTrue(models.Tweet.objects.exists())
