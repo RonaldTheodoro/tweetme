e5801c7014d9071f1af1c4f126ecf86e2fce502d
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-27 22:59:38 -0200

    check if context has a tweet instance

--- apps/tweet/test/test_tweet_detail.py
+++ apps/tweet/test/test_tweet_detail.py
@@ -1,14 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.urls import resolve, reverse
 from django.test import TestCase
 
 from rest_framework import status
 
-from .. import views
+from .. import views, models
+
+
+User = get_user_model()
+
 
 class TweetDetailViewTest(TestCase):
     url = reverse('tweet:detail', kwargs={'id': 1})
 
     def setUp(self):
+        user = User.objects.create_user(username='ronaldtheodoro')
+        self.tweet = models.Tweet.objects.create(
+            content='My First Tweet',
+            user=user
+        )
         self.response = self.client.get(self.url)
 
     def test_get(self):
@@ -20,3 +30,8 @@ class TweetDetailViewTest(TestCase):
     def test_function(self):
         index = resolve(self.url)
         self.assertEqual(views.tweet_detail, index.func)
+
+    def test_context(self):
+        self.assertIn('tweet', self.response.context)
+        self.assertIsInstance(self.response.context['tweet'], models.Tweet)
+        self.assertEqual(self.response.context['tweet'], self.tweet)
--- apps/tweet/views.py
+++ apps/tweet/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from . import models
 
 def index(request):
     return render(request, 'tweet/index.html', {})
 
 
 def tweet_detail(request, id):
-    return render(request, 'tweet/detail.html', {})
+    tweet = get_object_or_404(models.Tweet, id=id)
+    return render(request, 'tweet/detail.html', {'tweet': tweet})
 
 
 def tweet_list(request):
