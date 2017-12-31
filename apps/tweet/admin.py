e7d60354deacc2f13c63539561aaa0e97ec63841
ronald theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-31 19:48:51 -0200

    create TweetForm and add it to django admin

--- apps/tweet/admin.py
+++ apps/tweet/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from . import models
+from . import models, forms
+
+class TweetAdmin(admin.ModelAdmin):
+    form = forms.TweetForm
 
 
 admin.site.register(models.Tweet)
---
+++ apps/tweet/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from . import models
+
+class TweetForm(forms.ModelForm):
+
+    class Meta:
+        model = models.Tweet
+        fields = ('content', 'user', )
